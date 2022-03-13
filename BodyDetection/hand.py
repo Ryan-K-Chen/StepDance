0ad490f1b1b0618003004fdfb0b7186cfd800668
jpark955 <jpark955@gatech.edu>
2022-03-12 21:19:31 -0500

    change camera resolution support

--- BodyDetection/hand.py
+++ BodyDetection/hand.py
@@ -75,6 +75,28 @@ def serial_output(vib_val, head_tilt_val):
 
 cap = cv2.VideoCapture(0)
 
+def make_1080p():
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+
+def make_720p():
+    cap.set(3, 1280)
+    cap.set(4, 720)
+
+def make_480p():
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+def change_res(width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+# uncomment whatever resolution your camera supports
+# change_res()
+# make_1080p()
+# make_720p()
+# make_480p()
+
 # for hand detection
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False,
